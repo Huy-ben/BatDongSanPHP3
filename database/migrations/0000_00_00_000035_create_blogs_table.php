4698acc19dc05bac0b5682f99695629665b9a64d
Huy Ben <benpham06@gmail.com>
2026-04-07 12:46:53 +0700

    [Fix] Thêm category_id cho blog

--- database/migrations/0000_00_00_000035_create_blogs_table.php
+++ database/migrations/0000_00_00_000035_create_blogs_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -15,12 +14,14 @@ return new class extends Migration
             $table->id();
             $table->unsignedBigInteger('user_id')->comment('ID người dùng tạo bài viết');
             $table->string('title', 255)->comment('Tiêu đề bài viết');
+            $table->unsignedBigInteger('category_id')->comment('ID danh mục');
             $table->string('slug', 255)->unique()->comment('Đường dẫn thân thiện với SEO');
             $table->text('content')->comment('Nội dung bài viết');
             $table->string('image')->nullable()->comment('Hình ảnh đại diện của bài viết');
             $table->text('excerpt')->nullable()->comment('Trích đoạn ngắn của bài viết');
             $table->enum('status', ['draft', 'published'])->default('draft')->comment('Trạng thái bài viết');
             $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('category_id')->references('id')->on('categories');
             $table->timestamps();
         });
     }
